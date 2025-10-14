df466ae17b552557a5f6469aa5eb0d9f36df9243
OptionsTradingUni <christophermomsa@gmail.com>
2025-10-14 23:02:21 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // Enable class-based dark mode
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
