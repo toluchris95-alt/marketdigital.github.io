f1d5fdf2c07eadd9a69e4ab4dd00160d581f87fa
toluchris95-alt <toluchris95@gmail.com>
2025-10-15 01:33:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-// src/App.js
-
 import React from 'react';
-// The only change is on this line:
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
@@ -10,6 +7,7 @@ import { CartProvider } from './context/CartContext';
 import Navbar from './components/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
 
+// --- Pages ---
 import Home from './pages/Home';
 import ProductDetail from './pages/ProductDetail';
 import SignUp from './pages/SignUp';
@@ -18,29 +16,75 @@ import SellerDashboard from './pages/SellerDashboard';
 import OrderHistory from './pages/OrderHistory';
 import ProfilePage from './pages/ProfilePage';
 import CartPage from './pages/CartPage';
+import MessagesPage from './pages/MessagesPage'; // ✅ New import
 
 function App() {
   return (
     <AuthProvider>
       <ThemeProvider>
         <CartProvider>
-          {/* This <Router> tag correctly uses the HashRouter nickname from the import */}
           <Router>
-            <div className="dark:bg-gray-900 min-h-screen">
+            <div className="dark:bg-gray-900 min-h-screen flex flex-col">
               <Navbar />
-              <main className="container mx-auto px-4 py-8">
+              <main className="container mx-auto px-4 py-8 flex-1">
                 <Routes>
-                  {/* Public Routes */}
+                  {/* --- Public Routes --- */}
                   <Route path="/" element={<Home />} />
                   <Route path="/signup" element={<SignUp />} />
                   <Route path="/login" element={<Login />} />
                   <Route path="/product/:productId" element={<ProductDetail />} />
-                  
-                  {/* Protected Routes */}
-                  <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
-                  <Route path="/orders" element={<ProtectedRoute><OrderHistory /></ProtectedRoute>} />
-                  <Route path="/cart" element={<ProtectedRoute roles={['Buyer']}><CartPage /></ProtectedRoute>} />
-                  <Route path="/dashboard" element={<ProtectedRoute roles={['Seller']}><SellerDashboard /></ProtectedRoute>} />
+
+                  {/* --- Protected Routes --- */}
+                  <Route
+                    path="/profile"
+                    element={
+                      <ProtectedRoute>
+                        <ProfilePage />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="/orders"
+                    element={
+                      <ProtectedRoute>
+                        <OrderHistory />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="/cart"
+                    element={
+                      <ProtectedRoute roles={['Buyer']}>
+                        <CartPage />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="/dashboard"
+                    element={
+                      <ProtectedRoute roles={['Seller']}>
+                        <SellerDashboard />
+                      </ProtectedRoute>
+                    }
+                  />
+
+                  {/* --- Messaging Routes --- */}
+                  <Route
+                    path="/messages"
+                    element={
+                      <ProtectedRoute>
+                        <MessagesPage />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="/messages/:conversationId"
+                    element={
+                      <ProtectedRoute>
+                        <MessagesPage />
+                      </ProtectedRoute>
+                    }
+                  />
                 </Routes>
               </main>
             </div>
@@ -52,4 +96,3 @@ function App() {
 }
 
 export default App;
-
