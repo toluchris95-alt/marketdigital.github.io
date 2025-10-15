eb0ae2abc8c8ed90bb284ec9ae798de484341b11
toluchris95-alt <toluchris95@gmail.com>
2025-10-15 10:20:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-console.log("✅ React booting...");
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+try {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} catch (err) {
+  document.body.innerHTML = `
+    <div style="background:#111;color:#fff;padding:40px;font-family:monospace">
+      <h1>🚨 React failed to start</h1>
+      <p>${err.message}</p>
+      <pre>${err.stack}</pre>
+    </div>
+  `;
+}
