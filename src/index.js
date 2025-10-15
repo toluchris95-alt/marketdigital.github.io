5277982ce1821da618a7923a0651723fdb14f52f
toluchris95-alt <toluchris95@gmail.com>
2025-10-15 03:49:01 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log("✅ Root element found, rendering App");
 root.render(
   <React.StrictMode>
     <App />
