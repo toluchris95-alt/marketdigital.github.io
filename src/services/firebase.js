348355b62cc4fe4bce20003edbaf503d1603fe07
toluchris95-alt <toluchris95@gmail.com>
2025-10-15 01:21:31 +0100

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Replace this with your own Firebase configuration object
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB...",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  apiKey: "AIzaSyBV2GUGOfYBxjbAHjYaYwSyrCQP5ik0k0s",
+  authDomain: "digital-marketplace-4a9a6.firebaseapp.com",
+  projectId: "digital-marketplace-4a9a6",
+  storageBucket: "digital-marketplace-4a9a6.appspot.com",
+  messagingSenderId: "267883417599",
+  appId: "1:267883417599:web:f9bd92517a48dc1dcde1ca",
+  measurementId: "G-RR6Q1Q8L9B"
 };
 
 // Initialize Firebase
